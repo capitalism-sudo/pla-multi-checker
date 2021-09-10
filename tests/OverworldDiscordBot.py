c8831a66c6fff6754342d3c3a37d35a32d1af03e
JSB <90295772+jsb-pokemon@users.noreply.github.com>
2021-09-09 21:04:58 -0400

    First pass of tweaking changes based on PR feedback.

--- tests/OverworldDiscordBot.py
+++ tests/OverworldDiscordBot.py
@@ -1,22 +1,9 @@
-# Needs a discord bot account and a server with all the mark ids as emojis
-
-import asyncio
-import io
 import json
 import sys
-from functools import lru_cache
-from threading import Thread
-from time import sleep
-
-import discord
-import pokebase as pb
-from discord.ext import commands
-from discord.utils import get
-from PIL import Image
 
 sys.path.append('../')
-from discordbot import OverworldDiscordBot # Channels
+from discordbot import OverworldDiscordBot
 
 bot = OverworldDiscordBot()
 bot.configure(json.load(open("../config.json")))
-bot.run("ADD-TOKEN-HERE")
+bot.run(bot.config["DiscordToken"])
